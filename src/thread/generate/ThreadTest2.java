f13d8750c081f51504790423c97227997d19f87a
mic050r <s2216@e-mirim.hs.kr>
2023-01-03 14:35:41 +0900

    thread 연습

--- src/thread/generate/ThreadTest2.java
+++ src/thread/generate/ThreadTest2.java
@@ -4,13 +4,20 @@ public class ThreadTest2 implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Runnable을 사용한 Thread 실행 " + (i + 1));
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} // 0.2초
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ThreadTest2 t2 = new ThreadTest2();
+		Thread t3 = new Thread(t2); // new born 상태
+		t3.start(); // runnable 상태
 	}
 
 }
