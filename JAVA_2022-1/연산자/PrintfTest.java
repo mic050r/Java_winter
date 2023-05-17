39ea63fee4abf257d7d68eff8a10d48678f7fa88
林지현 <103114387+mic050r@users.noreply.github.com>
2023-05-17 23:10:27 +0900

    Delete JAVA_2022-1 directory

--- JAVA_2022-1/연산자/PrintfTest.java
+++
@@ -1,8 +0,0 @@
-public class PrintfTest{
-	public static void main(String args[]){
-		System.out.println("자바 \'입력 :");
-		System.out.println("인공지능 \"입력 :");
-		System.out.println("한국사 \\입력 :");
-		System.out.println("사회 \n입력 :");
-	}
-}
--- JAVA_2022-1/제어문/ArgsExam2.java
+++
@@ -1,8 +0,0 @@
-public class ArgsExam2{
-       public static void main(String args[]){
-	int n = args.length;
-	for(int i = 0; i < n; i++){
-	    System.out.println(args[i]);
-	}
-       }
-}
